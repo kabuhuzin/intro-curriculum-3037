eb1177b4637b661f09b1205a93ec4689d4210dcb
karasi <kabuhuzin@gmail.com>
2022-01-28 20:33:12 +0900

    非同期処理を同期処理に変更

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-  fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+
+
+async function main() {
+  for (let count = 0; count < 500; count++) {
+  //await　を使うには Promise　オブジェクトにしなければならない。
+  await new Promise((resolve) => {
+    //ここに非同期的な処理を書く
+    fs.appendFile(fileName, 'おはようございます', 'utf8', () => {
+      resolve()})//ファイル書き込みがおわったらresolveを実行
+  })
+  }
 }
+
+main()
